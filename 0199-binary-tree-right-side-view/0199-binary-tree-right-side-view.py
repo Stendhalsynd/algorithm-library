a8f82311a2a878c69019e8d07497385c13097300
jihun <qmflf556@naver.com>
2023-11-10 20:18:06 +0900

    Time: 47 ms (12.88%), Space: 16.4 MB (12.86%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.py
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.py
@@ -7,34 +7,47 @@ from collections import deque
 #         self.right = right
 class Solution:
     def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
-        if root is None:
-            return
+        answer = []
         
-        queue = deque([])
-        right_side = []
-        last_node = 0
-        
-        queue.append(root)
-        
-        queue.append(None)
-        
-        # BFS
-        while len(queue) > 0:
-            node = queue.popleft()
-            if node is None:
-                right_side.append(last_node)
-                queue.append(None)
-                
-                if queue[0] is None:
-                    break
-                else:
-                    continue
+        def dfs(node = root, level = 1):
+            if not node:
+                return
+            if len(answer) < level:
+                answer.append(node.val)
+            dfs(node.right, level + 1)
+            dfs(node.left, level + 1)
             
-            last_node = node.val
-            
-            if node.left is not None:
-                queue.append(node.left)
+        dfs()
+        
+        return answer
+#         if root is None:
+#             return
+        
+#         queue = deque([])
+#         right_side = []
+#         last_node = 0
+        
+#         queue.append(root)
+        
+#         queue.append(None)
+        
+#         # BFS
+#         while len(queue) > 0:
+#             node = queue.popleft()
+#             if node is None:
+#                 right_side.append(last_node)
+#                 queue.append(None)
                 
-            if node.right is not None:
-                queue.append(node.right)
+#                 if queue[0] is None:
+#                     break
+#                 else:
+#                     continue
+            
+#             last_node = node.val
+            
+#             if node.left is not None:
+#                 queue.append(node.left)
+                
+#             if node.right is not None:
+#                 queue.append(node.right)
         return right_side
