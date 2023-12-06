69ed5f7982d7b8ab3d7fec048e07ae23090c3bd7
jihun <qmflf556@naver.com>
2023-12-06 19:29:01 +0900

    Time: 1005 ms (17.75%), Space: 64.8 MB (52.03%) - LeetHub

--- 0077-combinations/0077-combinations.py
+++ 0077-combinations/0077-combinations.py
@@ -1,9 +1,25 @@
 from itertools import combinations
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
-        total = [val for val in range(1, n + 1)]
+#         total = [val for val in range(1, n + 1)]
         
-        cases = list(combinations(total, k))
-        answer = [list(combination) for combination in cases]
+#         cases = list(combinations(total, k))
+#         answer = [list(combination) for combination in cases]
         
-        return answer
+#         return answer
+
+        ans = []
+        nums = [val for val in range(1, n + 1)]
+        combination = []
+        def back(combination, nums):
+            if len(combination) == k:
+                ans.append(combination)
+            else:
+                for idx, num in enumerate(nums):
+                    if not combination:
+                        back([num], nums[idx:])
+                    elif combination and combination[-1] < num:
+                        back(combination + [num], nums[idx:])
+        back(combination, nums)
+        return ans
+                
