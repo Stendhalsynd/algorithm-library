50d0de95d140a42cee6e6c1eec05e3ea745b1c42
jihun <qmflf556@naver.com>
2023-12-11 19:51:24 +0900

    Time: 55 ms (21.40%), Space: 16.9 MB (11.47%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -1,7 +1,6 @@
 from bisect import bisect_left
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        # que = sum(matrix, [])
         return sum(matrix, []).count(target) > 0
         
         # for row in matrix:
